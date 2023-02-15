0de8c5b60da1a5fcb3c31ca6c088e5b4ee54c6d8
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-02-15 21:36:53 +0900

    ayaan : programmers / 다트게임 / Java / 2차

--- programmers/ayaan/week3/다트게임_ayaan.java
+++ programmers/ayaan/week3/다트게임_ayaan.java
@@ -2,7 +2,7 @@ package programmers.ayaan.week3;
 
 public class 다트게임_ayaan {
     public static void main(String[] args) {
-        String dart = "1S2D*3T";
+        String dart = "1D2S#10S";
         int result = solution(dart);
         System.out.println(result);
     }
@@ -10,79 +10,37 @@ public class 다트게임_ayaan {
     public static int solution(String dartResult){
         int answer = 0;
 
-        int preScore = 0;
-        int score = 0;
+        int[] scores = new int[3];
+        int turn = 0;
+        String num = "";
         for(int i=0; i<dartResult.length(); i++){
             char ch = dartResult.charAt(i);
-
             if(ch == 'S' || ch == 'D' || ch == 'T'){
+                int score = Integer.parseInt(num);
                 switch (ch){
                     case 'D':
                         score *= score;
                         break;
                     case 'T':
                         score = score * score * score;
-                        break;
                 }
+                scores[turn++] = score;
+                num = "";
             } else if (ch == '*' || ch == '#') {
                 switch (ch){
                     case '*':
-                        score *= 2;
-                        preScore *= 2;
+                        scores[turn-1] *= 2;
+                        if(turn > 1) scores[turn-2] *= 2;
                         break;
                     case '#':
-                        score *= -1;
+                        scores[turn-1] *= -1;
                         break;
                 }
             } else {
-                if(i != 0){
-                    preScore = score;
-                }
-                score = Integer.parseInt(Character.toString(ch));
-                continue;
-            }
-
-            answer += score;
-            answer += preScore;
-        }
-
-        return answer;
-    }
-
-    public static int solution2(String dartResult){
-        int answer = 0;
-
-        int[] scores = new int[3];
-        for(int i=0; i<3; i++){
-            int count = 0;
-            for(int j=0; j<3; j++){
-                switch (j){
-                    case 0:
-                        char first = dartResult.charAt(i+count);
-                        scores[j] = Integer.parseInt(Character.toString(first));
-                        count++;
-                        break;
-                    case 1:
-                        char second = dartResult.charAt(i+count);
-                        int bonus = scores[j];
-                        switch(second){
-                            case 'D':
-                                scores[j] = bonus * bonus;
-                                break;
-                            case 'T':
-                                scores[j] = bonus * bonus * bonus;
-                                break;
-                        }
-                        count++;
-                        break;
-                    case 2:
-
-                        count++;
-                        break;
-                }
+                num += ch;
             }
         }
-
+        answer = scores[0] + scores[1] + scores[2];
         return answer;
     }
 }
