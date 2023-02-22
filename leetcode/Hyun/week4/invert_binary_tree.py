404796d2d60dc63a2c0312d01fec22b48cb3e2f3
Hyun <ghkdgus29@naver.com>
2023-02-22 17:51:22 +0900

    Hyun : Leetcode/ invert_binary_tree / Python / 1차

--- leetcode/Hyun/week4/invert_binary_tree.py
+++ leetcode/Hyun/week4/invert_binary_tree.py
@@ -1,27 +1,23 @@
-# 입력으로 들어온 binary tree 를 invert
-# TreeNode 존재,
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
 
-        ans = []
+        if root is None:
+            return root
+
+        ans = root
         queue = [root]
         while queue:
             current_node = queue.pop(0)
-            ans.append(current_node.val)
-            if current_node.right:
+            tmp_left = tmp_right = None
+
+            if current_node.right is not None:
+                tmp_left = current_node.right
                 queue.append(current_node.right)
-            if current_node.left:
+            if current_node.left is not None:
+                tmp_right = current_node.left
                 queue.append(current_node.left)
 
+            current_node.right = tmp_right
+            current_node.left = tmp_left
+
         return ans
-
-
-
-
-
-
-
-
-
-
-
