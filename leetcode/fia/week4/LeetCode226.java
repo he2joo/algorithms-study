5f19188b1c6f976055bf8c5a072ea36aa832f0bd
yeon <peach-pie@naver.com>
2023-02-22 19:29:22 +0900

    Fia : LeetCode / 226 / Java / 풀이 실패

--- leetcode/fia/week4/LeetCode226.java
+++ leetcode/fia/week4/LeetCode226.java
@@ -1,47 +1,17 @@
 package leetcode.fia.week4;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LeetCode226 {
     public TreeNode invertTree(TreeNode root) {
-        List<TreeNode> prevDepth = new ArrayList<>();
-        prevDepth.add(root);
-        List<TreeNode> currentDepth = new ArrayList<>();
-        currentDepth.add(root);
-
-        TreeNode left;
-        TreeNode right;
-
-        while (currentDepth.size() != 0) {
-            for (int i = 0; i < currentDepth.size(); i++) {
-                TreeNode currentRoot = currentDepth.get(i);
-                if (currentRoot.left != null) {
-                    left = currentRoot.left;
-                } else {
-                    left = null;
-                }
-                if (currentRoot.right != null) {
-                    right = currentRoot.right;
-                } else {
-                    right = null;
-                }
-                currentRoot.left = right;
-                currentRoot.right = left;
-            }
-            prevDepth = currentDepth;
-            for (int i = 0; i < prevDepth.size(); i++) {
-                currentDepth = new ArrayList<>();
-                if (prevDepth.get(i).left != null) {
-                    currentDepth.add(prevDepth.get(i).left);
-                }
-                if (prevDepth.get(i).right != null) {
-                    currentDepth.add(prevDepth.get(i).right);
-                }
-            }
+        if (root == null) {
+            return null;
         }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
         return root;
     }
+
 }
 
 class TreeNode {
